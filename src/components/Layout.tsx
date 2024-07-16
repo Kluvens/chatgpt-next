57cc226bd8740e097fa8eed9a3a429df76c9e265
Kluvens <z5339252@ad.unsw.edu.au>
2024-07-16 21:11:38 +1000

    added layout and web analytics for the web

---
+++ src/components/Layout.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import Link from "next/link";
+import { ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>ChatGPT written in Next.js</title>
+        <meta name="description" content="A demo for ChatGPT in Next.js" />
+      </Head>
+      <header>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+          </ul>
+        </nav> */}
+        This is a header for ChatGPT-NEXT
+      </header>
+      <main>
+        {children}
+        <Analytics />
+      </main>
+      <footer>{/* <p>&copy; 2024 My Next.js App</p> */}</footer>
+    </>
+  );
+};
+
+export default Layout;
