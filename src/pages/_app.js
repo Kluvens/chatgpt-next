48707b525a92ded5ca8266c327ebb291c7ed9b12
Kluvens <z5339252@ad.unsw.edu.au>
2024-07-16 21:36:24 +1000

    added speed insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({
   Component,
@@ -10,6 +11,7 @@ export default function App({
     <SessionProvider session={session}>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </SessionProvider>
   );
 }
